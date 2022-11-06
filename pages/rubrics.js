d5744a530605e089dc5eafd519a0a2f35a2dbb91
Leroy Salih <leroy.salih@gmail.com>
2022-11-06 18:42:19 +0300

    adding revalidate

--- pages/rubrics.js
+++ pages/rubrics.js
@@ -6,14 +6,7 @@ import Rubric from '../components/rubric';
 
 import { getRubrics } from '../libs/getRubrics';
 
-import { CSVLink, CSVDownload } from "react-csv";
 
-const csvData = [
-    ["firstname", "lastname", "email"],
-    ["Ahmed", "Tomi", "ah@smthing.co.com"],
-    ["Raed", "Labes", "rl@smthing.co.com"],
-    ["Yezzi", "Min l3b", "ymin@cocococo.com"]
-  ];
 
 const Rubrics = ({rubrics}) => {
 
